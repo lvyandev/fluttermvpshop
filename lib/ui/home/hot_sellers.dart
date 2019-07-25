809430d18fbe0ce448a2dd3017b191558c104e1c
lvyan <lvyan2690330@163.com>
2019-07-25 11:44:47 +0800

    完成首页

--- lib/ui/home/hot_sellers.dart
+++ lib/ui/home/hot_sellers.dart
@@ -12,24 +12,29 @@ class HotSellers extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Chip(
-          backgroundColor: Colors.transparent,
-          labelPadding: EdgeInsets.all(0),
-          label: Text(
-            '火爆专区',
-            style: TextStyle(
-              fontSize: 10,
-            ),
-          ),
-          avatar: CircleAvatar(
-            backgroundColor: Colors.pink,
-            radius: 6,
-            child: Text(
-              '火',
+        Container(
+          height: 30,
+          width: double.infinity,
+          color: Colors.grey[200],
+          child: Chip(
+            backgroundColor: Colors.grey[200],
+            labelPadding: EdgeInsets.all(0),
+            label: Text(
+              '火爆专区',
               style: TextStyle(
-                color: Colors.white,
-                fontSize: 8,
-                textBaseline: TextBaseline.ideographic,
+                fontSize: 10,
+              ),
+            ),
+            avatar: CircleAvatar(
+              backgroundColor: Colors.pink,
+              radius: 6,
+              child: Text(
+                '火',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 8,
+                  textBaseline: TextBaseline.ideographic,
+                ),
               ),
             ),
           ),
@@ -38,7 +43,7 @@ class HotSellers extends StatelessWidget {
           shrinkWrap: true,
           physics: NeverScrollableScrollPhysics(),
           crossAxisCount: 2,
-          childAspectRatio: 2 / 3,
+          childAspectRatio: 1 / 1.21,
           children: _data.map((product) => _buildItem(product)).toList(),
         )
       ],
@@ -46,42 +51,72 @@ class HotSellers extends StatelessWidget {
   }
 
   Widget _buildItem(ProductBean product) {
-    return Column(
-      children: <Widget>[
-        Image.network(product.image),
-        Container(
-          width: double.infinity,
-          alignment: Alignment.center,
-          child: Text(
-            product.name,
-            style: TextStyle(
-              color: Colors.pink,
+    return Container(
+      padding: EdgeInsets.all(5),
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          Image.network(product.image),
+          Container(
+            width: double.infinity,
+            alignment: Alignment.center,
+            child: Text(
+              product.name,
+              style: TextStyle(
+                color: Colors.pink,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
             ),
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
           ),
-        ),
-        Flex(
-          direction: Axis.horizontal,
-          children: <Widget>[
-            Expanded(
-              flex: 2,
-              child: Text(
-                '￥${product.mallPrice}',
+          SizedBox(
+            height: 5,
+          ),
+          Flex(
+            direction: Axis.horizontal,
+            children: <Widget>[
+              Expanded(
+                flex: 1,
+                child: Text.rich(
+                  TextSpan(
+                    children: [
+                      _buildRmbLabelSpan(),
+                      TextSpan(
+                        text: '${product.mallPrice}',
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Text(
-                '￥${product.price}',
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                    decoration: TextDecoration.lineThrough, color: Colors.grey),
-              ),
-            ),
-          ],
-        )
-      ],
+              Expanded(
+                flex: 1,
+                child: Text.rich(
+                  TextSpan(
+                    style: TextStyle(
+                      decoration: TextDecoration.lineThrough,
+                      color: Colors.grey[350],
+                    ),
+                    children: [
+                      _buildRmbLabelSpan(),
+                      TextSpan(
+                        text: '${product.price}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
+
+  _buildRmbLabelSpan() => TextSpan(
+        text: '￥',
+        style: TextStyle(fontSize: 10),
+      );
 }
